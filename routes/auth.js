bf14704287c9978d84570fa39f006c567edaa210
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-05-06 23:47:53 +0900

    [Fix]: 주간분석 페이지 넘김 이슈 해결

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const passport = require("passport");
+const asyncHandler = require("../utils/async-handler");
 const { setUserToken } = require("../utils/jwt");
 
 const router = Router();
@@ -19,6 +20,14 @@ router.get(
     setUserToken(res, req.user); // jwt
     res.redirect("http://localhost:3001");
   }
+);
+
+router.get(
+  "/logout",
+  asyncHandler(async (req, res) => {
+    res.clearCookie("token");
+    res.redirect("/");
+  })
 );
 
 module.exports = router;
