e760c60bbda195ced16db70ccd418e9a81fb1022
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-06-15 21:27:37 +0900

    [Amend]: listen 메서드 제거

--- app.js
+++ app.js
@@ -50,8 +50,6 @@ app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 app.use("/time", timeRouter);
 
-app.listen(3000);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
