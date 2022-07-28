1438d0c925a42435cfa18de6aa9085299b766583
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-07-28 19:29:20 +0900

    [Amend]: res.json => res.send

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use("/auth", authRouter);
 app.use("/time", timeRouter);
 
 app.get("/getuser", (req, res) => {
-  res.json(req.user);
+  res.send(req.user);
 });
 
 app.get("/", (req, res) => {
